2c2d3f458fab1709334e2dab8af2732491eb23cc
GuilberValerio <102636307+GuilberValerio@users.noreply.github.com>
2024-07-16 03:36:51 -0300

    feat(todosJogos) Primeira Versão da página
    Primeira versão da página implementada, agora o código é capaz de:
    - Comunicar com o banco de Jogos
    - Criar novos Jogos predefinidos
    - Ler Todos os jogos cadastrados
    - Dispor o nome  dos Jogos cadastrados na tela em Cards

--- game_tracker_app/lib/paginas/todosJogos.dart
+++ game_tracker_app/lib/paginas/todosJogos.dart
@@ -1,5 +1,10 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:game_tracker_app/Jogo.dart';
+import 'package:game_tracker_app/controladores/ControladorJogos.dart';
+import 'package:game_tracker_app/helper/DatabaseHelper.dart';
 
 class TodosJogos extends StatefulWidget {
   static const String rota = "/todosJogos";
@@ -9,46 +14,154 @@ class TodosJogos extends StatefulWidget {
 }
 
 class _TodosjogosState extends State<TodosJogos> {
-  late SharedPreferences preferencias;
+  ControladorJogos ctrlJogos = ControladorJogos();
+  DatabaseHelper dbHelper = DatabaseHelper();
+
+  late Future<List<Jogo>> allGames;
+
+  int linhaJOgos = 4;
 
   @override
   void initState() {
     super.initState();
 
-    getPreferences();
+    allGames = incicializarTodosJogos();
   }
 
-  Future<void> getPreferences() async {
-    preferencias = await SharedPreferences.getInstance();
-    setState(() {});
+  Future<List<Jogo>> incicializarTodosJogos() async {
+
+    List<Jogo> jogos = await ctrlJogos.getTodosJogos();
+
+    if (jogos.isEmpty) {
+      await ctrlJogos.cadastrarJogo(Jogo(
+          id: 41,
+          user_id: 50,
+          name: "Link",
+          description: "Qualquer coisa",
+          release_date: "15/07/2024"));
+      await ctrlJogos.cadastrarJogo(Jogo(
+          id: 42,
+          user_id: 50,
+          name: "Mario",
+          description: "Qualquer coisa",
+          release_date: "15/07/2024"));
+      await ctrlJogos.cadastrarJogo(Jogo(
+          id: 43,
+          user_id: 50,
+          name: "Luigi",
+          description: "Qualquer coisa",
+          release_date: "15/07/2024"));
+      await ctrlJogos.cadastrarJogo(Jogo(
+          id: 44,
+          user_id: 50,
+          name: "Pokémon",
+          description: "Qualquer coisa",
+          release_date: "15/07/2024"));
+      await ctrlJogos.cadastrarJogo(Jogo(
+          id: 45,
+          user_id: 50,
+          name: "Pikachu",
+          description: "Qualquer coisa",
+          release_date: "15/07/2024"));
+      await ctrlJogos.cadastrarJogo(Jogo(
+          id: 46,
+          user_id: 50,
+          name: "LOL",
+          description: "Qualquer coisa",
+          release_date: "15/07/2024"));
+      await ctrlJogos.cadastrarJogo(Jogo(
+          id: 47,
+          user_id: 50,
+          name: "DOTA",
+          description: "Qualquer coisa",
+          release_date: "15/07/2024"));
+    }
+
+    jogos = await ctrlJogos.getTodosJogos();
+
+    return jogos;
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Todos os Jogos"),
-      ),
-      body: FutureBuilder(
-          future: getPreferences(),
-          builder: (Context, snapshot) {
+        appBar: AppBar(
+          title: Text("Todos os Jogos"),
+        ),
+        body: FutureBuilder<List<Jogo>>(
+          future: allGames,
+          builder: (context, snapshot) {
             if (snapshot.connectionState == ConnectionState.waiting) {
               return Center(child: CircularProgressIndicator());
             } else if (snapshot.hasError) {
-              return Center(child: Text('Erro ao carregar preferências'));
+                return Center(child: Text('Erro: ${snapshot.error}'));
+            } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+               return Center(child: Text('Nenhum jogo encontrado.'));
             } else {
-              return Container(
-                padding: EdgeInsets.all(15),
-                child: Column(
-                  children: [
-                    Text("teste"),
-                    Text(preferencias.getString("apelido") ??
-                        "Nenhum apelido definido")
-                  ],
-                ),
-              );
+                final jogos = snapshot.data!;
+                return Container(
+                  padding: EdgeInsets.all(15),
+                  child: Column(
+                    children: [
+                      Text("Lista de Jogos"),
+                      LayoutBuilder(
+                        builder: (context, constraints) {
+                          final itemSize = constraints.maxWidth / linhaJOgos;
+                          return AspectRatio(
+                            aspectRatio: 1,
+                            child: Container(
+                              padding: EdgeInsets.all(15),
+                              child: SingleChildScrollView(
+                                child: Column(
+                                  children: [
+                                    GridView.builder(
+                                      itemCount: jogos.length,
+                                      shrinkWrap: true,
+                                      physics: NeverScrollableScrollPhysics(),
+                                      gridDelegate:
+                                          SliverGridDelegateWithFixedCrossAxisCount(
+                                        crossAxisCount: linhaJOgos,
+                                        childAspectRatio: 1,
+                                      ),
+                                      itemBuilder: (BuildContext context, int index) {
+                                        return Card(
+                                          margin: EdgeInsets.all(5),
+                                          elevation: 5,
+                                          shape: RoundedRectangleBorder(
+                                            borderRadius: BorderRadius.circular(10),
+                                          ),
+                                          child: GestureDetector(
+                                            onTap: () {},
+                                            child: Container(
+                                              decoration:
+                                                  BoxDecoration(color: Colors.amber,borderRadius: BorderRadius.circular(10)),
+                                              margin: EdgeInsets.all(5),
+                                              width: itemSize,
+                                              //height: itemSize,
+                                              child: Center(
+                                                child: Text(jogos[index].name ?? 'Sem nome'),
+                                              ),
+                                            ),
+                                          ),
+                                        );
+                                      },
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          );
+                        },
+                      ),
+                    ],
+                  )
+                );
             }
-          }),
-    );
+          }
+ 
+        )
+          
+      );
+      
   }
 }
