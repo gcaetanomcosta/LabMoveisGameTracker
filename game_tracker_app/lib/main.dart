58ce9e31252269675b1e3dbc3472aae976419c07
GuilberValerio <102636307+GuilberValerio@users.noreply.github.com>
2024-07-16 03:47:26 -0300

    feat(main) incializar DB
    Agora todas as tabelas do banco de dados são corretamente criadas quando o aplicativo abre

--- game_tracker_app/lib/main.dart
+++ game_tracker_app/lib/main.dart
@@ -4,9 +4,18 @@ import 'package:game_tracker_app/paginas/cadastro.dart';
 import 'package:game_tracker_app/paginas/meusJogos.dart';
 import 'package:game_tracker_app/paginas/login.dart';
 import 'package:game_tracker_app/paginas/todosJogos.dart';
+import 'package:game_tracker_app/helper/DatabaseHelper.dart';
 
 
-void main() {
+void main() async {
+
+  WidgetsFlutterBinding.ensureInitialized();
+  try {
+    await DatabaseHelper().db;
+  } catch (e) {
+    print('Erro ao inicializar o banco de dados: $e');
+    // Você pode adicionar lógica para lidar com o erro, como exibir uma mensagem ou encerrar o aplicativo
+  }
   runApp(
     MaterialApp(
       title: "Game Tracker",
