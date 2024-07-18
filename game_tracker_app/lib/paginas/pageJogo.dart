f23c8c24c90bbc115a32bf6a8a8a007216c8f78e
GuilberValerio <102636307+GuilberValerio@users.noreply.github.com>
2024-07-18 04:21:27 -0300

    feat/fix(pageJogo) pagina criada e consertada
    - Julia desenvolveu a página que mostra as informações de cada jogo
    
    - Eu corrigi o erro de Overflow da lista de Reviews
    
    - Parte do código que chamava a função de recolher a média das reviews foi comentada para que eu pudesse rodar o projeto
    LEMBRAR DE DESCOMENTAR

--- game_tracker_app/lib/paginas/pageJogo.dart
+++ game_tracker_app/lib/paginas/pageJogo.dart
@@ -3,12 +3,10 @@ import 'package:game_tracker_app/Jogo.dart';
 import 'package:game_tracker_app/Review.dart';
 import 'package:game_tracker_app/controladores/ControladorJogos.dart';
 import 'package:game_tracker_app/controladores/ControladorReview.dart';
-
-
-import 'package:game_tracker_app/paginas/adicionarResenha.dart';
+import 'package:game_tracker_app/paginas/adicionaResenha.dart';
 
 class PageJogo extends StatefulWidget {
- 
+  static const String rota = "/pageJogo";
   Jogo jogoSelecionado;
     
   PageJogo(this.jogoSelecionado); 
@@ -23,6 +21,12 @@ class _PageJogoState extends State<PageJogo> {
     ControladorReview ctrlResenha = ControladorReview();
     return ctrlResenha.getReviewsJogo(game_id);
   }
+/*
+  Future<double?> getMediaResenhas() async{
+    int game_id = widget.jogoSelecionado.id;
+    ControladorReview ctrlResenha = ControladorReview();
+    return ctrlResenha.getMediaReviews(game_id);
+  }*/
 
   @override
   Widget build(BuildContext context) {
@@ -32,149 +36,160 @@ class _PageJogoState extends State<PageJogo> {
             backgroundColor: Colors.amber,
         ),
         body: SafeArea(
-            child: Column(
-                children: [ 
-                    Card(
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(4),
-                        ),
-                        clipBehavior: Clip.antiAliasWithSaveLayer,
-
-                        child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: <Widget>[
-                                Image.asset(
-                                    'lib/assets/Key-bro.png', 
-                                    height: 200,
-                                    width: double.infinity,
-                                    fit: BoxFit.cover,
-                                ),
-                                Container(
-                                    padding: const EdgeInsets.fromLTRB(15, 15, 15, 0),
-                                    child: Column(
-                                        crossAxisAlignment: CrossAxisAlignment.start,
-                                        children: <Widget> [
-                                            //Nome do Jogo
-                                            Text(
-                                                "${widget.jogoSelecionado.name}", 
-                                                style: TextStyle(
-                                                    fontSize: 20,
-                                                    color: Colors.purple,
-                                                ),
-                                            ),
-                                            
-                                            SizedBox(height: 30),
-                                        
-                                            //Data 
-                                            Text(
-                                                "Data de Lançamento: ${widget.jogoSelecionado.release_date}",
-                                                style: TextStyle(
-                                                    fontSize: 18,
-                                                    color: Colors.grey[800],
-                                                ),
-                                            ),
-
-                                            SizedBox(height: 25),
-                                            //Descrição
-                                            Text(
-                                                "${widget.jogoSelecionado.description}",
-                                                style: TextStyle(
-                                                    fontSize: 16,
-                                                    color: Colors.grey[800],
-                                                ),
-                                            ),
-
-                                            Row(
-                                                children: <Widget>[
-                                                    //Trocar por nota media
-                                                    Text(
-                                                        "Nota Média: ${widget.jogoSelecionado.name}",
-                                                        style: TextStyle(
-                                                            fontSize: 16,
-                                                            color: Colors.grey[800],
-                                                        ),
-                                                    ),
-                                                
-                                                ],
-                                            ),
-                                        ],
-                                    ),
-                                ),
-
-                            ],
-
-                        ),
-                    ),
-
-                   SizedBox(height: 10),
-                    //Botao de adicionar review e adicionar rota
-                    SizedBox(
-                        width: 200,
-                        child: ElevatedButton(
-                        onPressed: () => Navigator.pushNamed(context, AdicionarResenha.rota),
-                        child: Text("Adicionar Review")
-                        ),
-                    ),
-
-                    SizedBox(height: 30),
-
-                    //Adicionar reviews recentes somente SE ------------------------------ tiver reviews
-                    Text(
-                        "REVIEWS RECENTES",
-                        style: TextStyle(
-                        fontSize: 18,
-                        color: Colors.grey[500],
-                        ),
-                    ),
-
-                    //Usando modelo do alerta dengue Lista de reviews
-                    
-                    FutureBuilder(
-                        future: getResenhasJogo(),
-                        builder: (context, snapshot) {
-                        switch (snapshot.connectionState) {
-                            case ConnectionState.none:
-                            case ConnectionState.waiting:
-                                return Center(child: CircularProgressIndicator());
-                            case ConnectionState.active:
-                            case ConnectionState.done:
-                                if (snapshot.hasError) {
-                                    print("Erro ao carregar");
-                                    break;
-                                } else {
-                                    //List<Review>? resenhas = snapshot.data?.cast<Review>();
-                                    dynamic resenhas = getReviewsJogo();
-
-                                    return Expanded(
-                                        flex: 2,
-                                        child: ListView.builder(
-                                            
-                                            itemCount: resenhas!.length,
-                                            itemBuilder: (context, index) {
-                                                Review review = resenhas[index];
-                                                //dynamic num = review.SE;
-
-                                                return Card(
-                                                    child: ListTile(
-                                                    title: Text('Resenha $review.id'),
-                                                    subtitle: Text(
-                                                        'User: $review.user_id\n Nota: $ review.score \n Descrição: $review.description \n'),
-                                                    isThreeLine: true,
-                                                    ),
-                                                );
-                                            }
-                                        )
-                                    );
-                                }
-                           
-                            return Text(
-                                "Erro ao carregar. Verifique se todos os dados foram preenchidos");
-                            });
-                        }
-                    }
-            
-        
-                ]
+            child: SingleChildScrollView(
+              child: Column(
+                  children: [ 
+                      Card(
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(4),
+                          ),
+                          clipBehavior: Clip.antiAliasWithSaveLayer,
+              
+                          child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: <Widget>[
+                                  Image.asset(
+                                      'lib/assets/${widget.jogoSelecionado.id}.png', 
+                                      height: 200,
+                                      width: double.infinity,
+                                      fit: BoxFit.fitHeight,
+                                  ),
+                                  Container(
+                                      padding: const EdgeInsets.fromLTRB(15, 15, 15, 0),
+                                      child: Column(
+                                          crossAxisAlignment: CrossAxisAlignment.start,
+                                          children: <Widget> [
+                                              //Nome do Jogo
+                                              Text(
+                                                  "${widget.jogoSelecionado.name}", 
+                                                  style: TextStyle(
+                                                      fontSize: 20,
+                                                      color: Colors.purple,
+                                                  ),
+                                              ),
+                                              
+                                              SizedBox(height: 30),
+                                          
+                                              //Data 
+                                              Text(
+                                                  "Data de Lançamento: ${widget.jogoSelecionado.release_date}",
+                                                  style: TextStyle(
+                                                      fontSize: 18,
+                                                      color: Colors.grey[800],
+                                                  ),
+                                              ),
+              
+                                              SizedBox(height: 25),
+                                              //Descricao
+                                              Text(
+                                                  "${widget.jogoSelecionado.description}",
+                                                  style: TextStyle(
+                                                      fontSize: 16,
+                                                      color: Colors.grey[800],
+                                                  ),
+                                              ),
+                                              
+                                              SizedBox(height: 25),
+              
+                                              Row(
+                                                  children: <Widget>[
+                                                      //Trocar por nota media das resenhas -------------------------------
+                                                      /*Text(
+                                                          "Nota Média: ${getMediaResenhas()}",
+                                                          style: TextStyle(
+                                                              fontSize: 16,
+                                                              color: Colors.grey[800],
+                                                          ),
+                                                      ),*/
+                                                  ],
+                                              ),
+                                              SizedBox(height: 25),
+                                          ],
+                                      ),
+                                  ),
+                              ],
+                          ),
+                      ),
+              
+                     SizedBox(height: 10),
+                      //Botao de adicionar review e adicionar rota
+                      SizedBox(
+                          width: 200,
+                          child: ElevatedButton(
+                          onPressed: /*() => Navigator.pushNamed(context, AdicionaResenha.rota),*/
+                          (){Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                        builder: (context) => AdicionaResenha(widget.jogoSelecionado),
+                                      ),
+                                    );},
+                          child: Text("Adicionar Review")
+                          ),
+                      ),
+              
+                      SizedBox(height: 30),
+              
+                      //Adicionar reviews recentes somente SE ------------------------------ tiver reviews
+                      Text(
+                          "REVIEWS RECENTES",
+                          style: TextStyle(
+                          fontSize: 18,
+                          color: Colors.grey[500],
+                          ),
+                      ),
+                     
+                      FutureBuilder(
+                          future: getResenhasJogo(),
+                          builder: (context, snapshot) {
+                              switch (snapshot.connectionState) {
+                                  case ConnectionState.none:
+                                  case ConnectionState.waiting:
+                                      return Center(child: CircularProgressIndicator());
+                                  case ConnectionState.active:
+                                  case ConnectionState.done:
+                                      if (snapshot.hasError) {
+                                          print("Erro ao carregar");
+                                          break;
+                                      } 
+                                     
+                                      else {
+                                          List<Review>? resenhas = snapshot.data?.cast<Review>();
+                                          //dynamic resenhas = getResenhasJogo();
+                                          return Column(
+                                            children: [
+                                              ListView.builder(
+                                                  shrinkWrap: true,
+                                                  itemCount: resenhas!.length,
+                                                  itemBuilder: (context, index) {
+                                                      Review rev = resenhas[index];
+                                                      dynamic user_id = rev.user_id;
+                                                      dynamic score = rev.score;
+                                                      dynamic descr = rev.description;
+                                                      dynamic resenha_id = rev.id;
+                                                            
+                                                      return Card(
+                                                          child: ListTile(
+                                                          title: Text('Resenha $resenha_id'),
+                                                          subtitle: Text(
+                                                              'User: $user_id.\n Nota: $score \n Descrição: $descr \n'),
+                                                          isThreeLine: true,
+                                                          ),
+                                                      );
+                                                  }
+                                              ),
+                                            ],
+                                          );     
+                                      }
+                                  }
+                             
+                                  return Text(
+                                      "Erro ao carregar"
+                                  );
+                                  
+                          }
+                      )
+                  ]
+              ),
             )
         )
     );
